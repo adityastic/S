fbb123b88f69d806b4ff5d1f0fb546efc6124f7c
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-11-24 23:49:07 +0000

    Update windows-7.ps1

--- scripts/windows-7.ps1
+++ scripts/windows-7.ps1
@@ -1,2 +1,2 @@
-(New-Object System.Net.WebClient).DownloadFile('https://nodejs.org/download/release/v12.20.0/node-v12.20.0-x64.msi', “$env:temp\node.msi”)
+(New-Object System.Net.WebClient).DownloadFile('https://nodejs.org/download/release/v12.20.0/node-v12.20.0-x64.msi', \“$env:temp\node.msi\”)
 (Start-Process "msiexec.exe" -ArgumentList "/i `"$env:temp\node.msi`" /qn" -NoNewWindow -Wait -PassThru).ExitCode
