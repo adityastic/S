bce029e856a4df70f8f224fb66cf36b4a4b7f940
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-11-24 23:30:20 +0000

    Update windows-7.ps1

--- scripts/windows-7.ps1
+++ scripts/windows-7.ps1
@@ -1,4 +1,2 @@
-# iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/adityastic/hw-info-cli/master/scripts/windows.ps1'))
-
 (New-Object System.Net.WebClient).DownloadFile('https://nodejs.org/dist/latest-v12.x/node-v12.19.1-x64.msi',` “$env:temp\node.msi”)
 (Start-Process "msiexec.exe" -ArgumentList "/i `"$env:temp\node.msi`" /qn" -NoNewWindow -Wait -PassThru).ExitCode
