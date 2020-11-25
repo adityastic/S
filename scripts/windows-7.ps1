a4037043c9f030456b456be3d988d353a444ea2d
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-11-25 08:54:56 +0000

    Update windows-7.ps1

--- scripts/windows-7.ps1
+++ scripts/windows-7.ps1
@@ -3,7 +3,10 @@
 $url = 'https://nodejs.org/download/release/v12.20.0/node-v12.20.0-x86.msi'
 if ((Get-WmiObject win32_operatingsystem | select osarchitecture).osarchitecture -like "64*")
 {
+  Write-Host 'Found x64 Arch'
   $url = 'https://nodejs.org/download/release/v12.20.0/node-v12.20.0-x64.msi'
 }
+Write-Host 'Downloading Node'
 (New-Object System.Net.WebClient).DownloadFile($url, "$env:temp\node.msi")
+Write-Host 'Installing Node'
 (Start-Process "msiexec.exe" -ArgumentList "/i `"$env:temp\node.msi`" /qn" -NoNewWindow -Wait -PassThru).ExitCode
